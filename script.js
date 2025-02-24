b56953acf3b0508698045e2d1be7b29392584d43
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-24 16:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const Text=document.querySelector("#status");
+const Button=document.querySelector("#enterBtn");
+const heading=document.createElement("h1");
+heading.innerHTML="Entered Metaverse";
+Button.addEventListener('click',()=>{
+	Text.parentNode.replaceChild(heading,Text);
+})
