6fda8de19529bd58bc6db4bb221d17f7f1f784af
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-24 21:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //your JS code here. If required.
-const status=document.getElementById("status");
+const statusParagraph=document.getElementById("status");
 const enterBtn=document.getElementById("enterBtn");
 
 enterBtn.addEventListener('click',()=>{
-     const heading=document.createElement("h1");
-    statusParagraph.innerHTML = "<h1 id="status">Entered Metaverse</h1>";
-	status.parentNode.replaceChild(heading,status);
+    statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
 })
